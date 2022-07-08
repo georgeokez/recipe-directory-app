4ae35f9435ef76b2edb851d6a7f31adc0301968f
georgeokez <georgeokez@gmail.com>
2022-07-08 18:08:10 +0100

    chore: added navbar ui

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <nav>
-            NavBar
+        <nav className="nav-bar-container">
+            <nav className="nav-bar">
+                <h1>Cooking Ninja</h1>
+                <div className="nav-bar-menu-container ">
+                    <label className="search-bar-label" for="search">Search:</label>
+                    <input type="text" id="search" />
+                    <button onClick={() => console.log('Clicking Create Recipe btn')}>Create Recipe</button>
+                </div>
+            </nav>
         </nav>
     );
 }
